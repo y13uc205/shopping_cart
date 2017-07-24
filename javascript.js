c14dff71f9f50123d39939317a7d107900bb0ad4
Shubham Gupta <samigupta248@gmail.com>
2017-07-24 19:26:06 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-alert("Hello, you may be the chosen one.");
+// alert("Hello, you may be the chosen one.");
+var count = 1;
+
+function markFavorite(class_name) {
+    // alert("Thuis is working");
+
+    var property = document.getElementsByClassName(class_name);
+    if (count==1) {
+        property[0].style.backgroundColor = "#7FFF00";
+        count=0;
+    }
+    else{
+        property[0].style.backgroundColor = "#808080";
+        count=1;
+    }
+    
+}
+
+function addToCart(class_name){
+
+}
+
+function computeTotal(class_name){
+
+}
