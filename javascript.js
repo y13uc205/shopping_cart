c5efa15ba51b81647867035f7f1840225a2c3bc2
Shubham Gupta <samigupta248@gmail.com>
2017-08-11 19:17:13 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // alert("Hello, you may be the chosen one.");
-var count = 0;
+//var count = 0;
 
 
 
@@ -7,15 +7,14 @@ var count = 0;
 
 function markFavorite(class_name) {
     // alert("Thuis is working");
-
     var property = document.getElementsByClassName(class_name);
-    if (count==1) {
-        property[0].style.backgroundColor = "#7FFF00";
-        count=0;
+    if (localStorage.count===undefined || localStorage.count==0) {
+        property[0].style.backgroundColor = "#ECF0F1";
+        localStorage.count=1;
     }
     else{
-        property[0].style.backgroundColor = "#808080";
-        count=1;
+        property[0].style.backgroundColor = "#7FFF00";
+        localStorage.count=0;
     }
     
 }
@@ -41,34 +40,59 @@ function addToCart(index){
 // if(localStorage.cart !== undefined){
 //         document.getElementById("demo_cart").innerHTML = localStorage.cart;
 //     }
-
+//debugger;
 function shoppingCart() {
+    if(localStorage.cart===undefined){
+        localStorage.cart=undefined;
+    }
+    
     var xmlhttp2 = new XMLHttpRequest();
     xmlhttp2.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj2 = JSON.parse(this.responseText);
         var display = "";
-        var quantity = [0,0,0];
+        var itemLength = myObj2.name.length;
+        var quantity = [];
+        for(var i = 0; i<itemLength; i++){
+            quantity.push(0);
+        }
         var total = 0;
-        
-        for(var k=0;k<localStorage.cart.length; k++){
-            var t= parseInt(localStorage.cart[k]);
-            quantity[t]++;
+        if (localStorage.cart=="undefined"){
+            clearCart();
         }
-        //alert(quantity);
-        
-        for(var p=0; p<quantity.length; p++){
-            display += "<div id=" + "box2" + "><p>" + "Name: " + myObj2.name[p] + "<br>"
-                + "Price: " + myObj2.price[p]  + "<br>"
-                + "Quantity: " + quantity[p]   + "<br>"
-                + "Cost: " + quantity[p]*myObj2.price[p]  + "</p>" + "<br>"
-                + "</div>";
-            total += quantity[p]*myObj2.price[p];
-        }
-        display += "<div><button onclick=" + "checkOut()" + ">CheckOut</button></div>"
-        localStorage.totalAmount = total;
-        document.getElementById("demo_cart").innerHTML = display;
-}
+        else{
+            for(var k=0;k<localStorage.cart.length; k++){
+                var t= parseInt(localStorage.cart[k]);
+                quantity[t]++;
+            }
+            //alert(quantity);
+            
+            for(var p=0; p<quantity.length; p++){
+                if(quantity[p]==0){
+                    continue;
+                }
+                else{
+                    display += "<div class=" + "products" + "><p>" + "Name: " + myObj2.name[p] + "<br>"
+                    + "Price: " + myObj2.price[p]  + "<br>"
+                    + "Quantity: " + quantity[p]   + "<br>"
+                    + "Cost: " + quantity[p]*myObj2.price[p]  + "</p>" + "<br>"
+                    + "</div>";
+                total += quantity[p]*myObj2.price[p];
+                }
+                
+                
+                // display += "<div class=" + "products" + "><p>" + "Name: " + myObj2.name[p] + "<br>"
+                //     + "Price: " + myObj2.price[p]  + "<br>"
+                //     + "Quantity: " + quantity[p]   + "<br>"
+                //     + "Cost: " + quantity[p]*myObj2.price[p]  + "</p>" + "<br>"
+                //     + "</div>";
+                // total += quantity[p]*myObj2.price[p];
+            }
+            display += "<div><button onclick=" + "checkOut()" + ">CheckOut</button></div>"
+            localStorage.totalAmount = total;
+            document.getElementById("demo_cart").innerHTML = display;
+    }
+    }
 };
 xmlhttp2.open("GET", "https://raw.githubusercontent.com/shubh276/Olivanders/master/products.json", true);
 xmlhttp2.send();
@@ -78,15 +102,20 @@ function checkOut() {
     alert("Total payable amount is "+localStorage.totalAmount);
 }
 
+function clearCart() {
+    localStorage.cart = undefined;
+    document.getElementById("demo_cart").innerHTML = "You have Zero items in your cart."
+}
 
-//fetching json data
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
+function detailsOnload(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
         //var result1 = "";
         var result = "";
-        for(var i=0; i<myObj.name.length;i++){
+        var i = parseInt(localStorage.detail);
+        
             //result1 += myObj.name[i] + "  " + myObj.price[i] + "<br>";
             result += "<div id=" + "box" + "><p>" + "Name: " + myObj.name[i] + "<br>"
             + "Previous Owner: " + myObj.previous_owner[i] + "<br>"
@@ -97,8 +126,44 @@ xmlhttp.onreadystatechange = function() {
             
             + "<button class=" + "favorite" + i + " onclick=" + "markFavorite('favorite" + i + "')" + ">Favorite</button>"
             + "   "
-            
             + "<button class=" + "addtocart" + i + " onclick=" + "addToCart(" + i + ")>Add To Cart</button>"
+            + "<a href=" + "details.html" + " onclick=" + "detailsVar(" + i + "); " + ">Details</a>"
+            + "</div>";
+        
+        
+        document.getElementById("demo_details").innerHTML = result;
+    }
+};
+xmlhttp.open("GET", "https://raw.githubusercontent.com/shubh276/Olivanders/master/products.json", true);
+xmlhttp.send();
+    //document.getElementById("demo_details").innerHTML= localStorage.detail;
+   
+}
+
+function detailsVar(index){
+    localStorage.detail = index;
+}
+
+
+//fetching json data
+//debugger;
+function indexOnload(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        //var result1 = "";
+        var result = "";
+        for(var i=0; i<myObj.name.length;i++){
+            //result1 += myObj.name[i] + "  " + myObj.price[i] + "<br>";
+            result += "<div class=" + "products"  + "><h2>"  + myObj.name[i] + "</h2><p>"
+            + "<img src=" + myObj.wand[i] + " width=" + "150px" + " height=" + "150px" + "><br>"
+            + "<h3>Price: " + myObj.price[i]  + "</p>" + "<h3>"
+            
+            + "<button class=" + "favorite" + i + " onclick=" + "markFavorite('favorite" + i + "')" + ">Favorite</button>"
+            + "   "
+            + "<button class=" + "addtocart" + i + " onclick=" + "addToCart(" + i + ")>Add To Cart</button><br>"
+            + "<br><a href=" + "details.html" + " onclick=" + "detailsVar(" + i + "); " + ">More Details</a>"
             + "</div>";
         }
         
@@ -107,6 +172,8 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open("GET", "https://raw.githubusercontent.com/shubh276/Olivanders/master/products.json", true);
 xmlhttp.send();
+}
+
 
 
 
