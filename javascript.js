4268c0379325d215b4c25f0925576e5d1e139ce3
Shubham Gupta <samigupta248@gmail.com>
2017-08-14 08:26:44 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,21 +1,31 @@
 // alert("Hello, you may be the chosen one.");
-//var count = 0;
+var count = 0;
 
 
 
 // document.cookie = 
 
-function markFavorite(class_name) {
+function markFavorite(id_name) {
     // alert("Thuis is working");
-    var property = document.getElementsByClassName(class_name);
-    if (localStorage.count===undefined || localStorage.count==0) {
-        property[0].style.backgroundColor = "#ECF0F1";
-        localStorage.count=1;
+    // alert(id_name);
+    var property = document.getElementById(id_name);
+    
+    if(count==0){
+        property.style.backgroundColor= "#7FFF00";
+        count=1;
     }
     else{
-        property[0].style.backgroundColor = "#7FFF00";
-        localStorage.count=0;
+        property.style.backgroundColor= "#deb887";
+        count=0;
     }
+    // if (localStorage.count===undefined || localStorage.count==0) {
+    //     property.style.backgroundColor = "#ECF0F1";
+    //     localStorage.count=1;
+    // }
+    // else{
+    //     property[0].style.backgroundColor = "#7FFF00";
+    //     localStorage.count=0;
+    // }
     
 }
 
@@ -66,29 +76,24 @@ function shoppingCart() {
                 quantity[t]++;
             }
             //alert(quantity);
-            
+            display += "<div  class="+"cartTable"+" ><table id="+"t01"+" ><tr><th>Item</th><th>Price</th><th>Quantity</th><th>Cost</th></tr>";
+
             for(var p=0; p<quantity.length; p++){
                 if(quantity[p]==0){
                     continue;
                 }
                 else{
-                    display += "<div class=" + "products" + "><p>" + "Name: " + myObj2.name[p] + "<br>"
-                    + "Price: " + myObj2.price[p]  + "<br>"
-                    + "Quantity: " + quantity[p]   + "<br>"
-                    + "Cost: " + quantity[p]*myObj2.price[p]  + "</p>" + "<br>"
-                    + "</div>";
+                    display += "<tr><td>"+ myObj2.name[p] + "</td><td>"
+                                         + myObj2.price[p] + "</td><td>" 
+                                         + quantity[p] + "</td><td>" 
+                                         + quantity[p]*myObj2.price[p] +"</td></tr>";
                 total += quantity[p]*myObj2.price[p];
                 }
-                
-                
-                // display += "<div class=" + "products" + "><p>" + "Name: " + myObj2.name[p] + "<br>"
-                //     + "Price: " + myObj2.price[p]  + "<br>"
-                //     + "Quantity: " + quantity[p]   + "<br>"
-                //     + "Cost: " + quantity[p]*myObj2.price[p]  + "</p>" + "<br>"
-                //     + "</div>";
-                // total += quantity[p]*myObj2.price[p];
             }
-            display += "<div><button onclick=" + "checkOut()" + ">CheckOut</button></div>"
+            display += "</table></div>";
+            display += "<div class="+ "checkout"+"><button class="+"button" +" onclick="+"clearCart()"+">Clear Cart</button>"
+                        + "<span >  Shubham   Gupta  </span>"
+                        + "<button  class="+"button" +" onclick=" + "checkOut()" + ">CheckOut</button></div>";
             localStorage.totalAmount = total;
             document.getElementById("demo_cart").innerHTML = display;
     }
@@ -104,7 +109,7 @@ function checkOut() {
 
 function clearCart() {
     localStorage.cart = undefined;
-    document.getElementById("demo_cart").innerHTML = "You have Zero items in your cart."
+    document.getElementById("demo_cart").innerHTML = "<div class=" + "zero"+">You have Zero items in your cart.</div>"
 }
 
 function detailsOnload(){
@@ -117,17 +122,14 @@ function detailsOnload(){
         var i = parseInt(localStorage.detail);
         
             //result1 += myObj.name[i] + "  " + myObj.price[i] + "<br>";
-            result += "<div id=" + "box" + "><p>" + "Name: " + myObj.name[i] + "<br>"
+            result += "<div class="+"detailImage"+ "><img src=" + myObj.wand[i] + "></div>" 
+            + "<div class=" + "description" + "><p>" + "Name: " + myObj.name[i] + "<br>"
             + "Previous Owner: " + myObj.previous_owner[i] + "<br>"
             + "Length: " + myObj.length[i] + "<br>"
-            + "Material Used: " + myObj.madeOf[i] + "<br>"
+            + "Material: " + myObj.madeOf[i] + "<br>"
             + "Previous Owner: " + myObj.previous_owner[i] + "<br>"
             + "Price: " + myObj.price[i]  + "</p>" + "<br>"
-            
-            + "<button class=" + "favorite" + i + " onclick=" + "markFavorite('favorite" + i + "')" + ">Favorite</button>"
-            + "   "
-            + "<button class=" + "addtocart" + i + " onclick=" + "addToCart(" + i + ")>Add To Cart</button>"
-            + "<a href=" + "details.html" + " onclick=" + "detailsVar(" + i + "); " + ">Details</a>"
+            + "<button  class="+"button" +"  onclick=" + "addToCart(" + i + ")>Add To Cart</button>"
             + "</div>";
         
         
@@ -160,9 +162,9 @@ function indexOnload(){
             + "<img src=" + myObj.wand[i] + " width=" + "150px" + " height=" + "150px" + "><br>"
             + "<h3>Price: " + myObj.price[i]  + "</p>" + "<h3>"
             
-            + "<button class=" + "favorite" + i + " onclick=" + "markFavorite('favorite" + i + "')" + ">Favorite</button>"
+            + "<button id=" + "favorite" + i + " class="+"button" +"  onclick=" + "markFavorite('favorite" + i + "')" + ">Favorite</button>"
             + "   "
-            + "<button class=" + "addtocart" + i + " onclick=" + "addToCart(" + i + ")>Add To Cart</button><br>"
+            + "<button  class="+"button" +"  onclick=" + "addToCart(" + i + ")>Add To Cart</button><br>"
             + "<br><a href=" + "details.html" + " onclick=" + "detailsVar(" + i + "); " + ">More Details</a>"
             + "</div>";
         }
